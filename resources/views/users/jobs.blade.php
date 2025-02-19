9e14c9c5b1da9d1670f2de5766520062919b4edb
rahim8050 <rahimranxx8050@gmail.com>
2025-02-19 06:19:29 +0000

    ins

--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -1,21 +1,56 @@
 {{-- @include('partials.search') --}}
 @include('users.partials.hero')
 
-<x-card>
+{{-- <x-card> --}}
     @unless (count($jobs) == 0)
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($jobs as $job)
-                <a href="/jobs/{{$job['id']}}">
-                    <h2>{{$job['title']}}</h2>
-                </a>
-                <p>{{$job['tags']}}</p>
-                <p class="text-success font-mono 5rem">{{$job['description']}}</p>
+                  <!-- Item 1 -->
+                  <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex border-b border-gray-200 pb-4 mb-4">
+                        <img
+                            class="hidden w-24 h-24 mr-6 md:block"
+                            src="images/acme.png"
+                            alt=""
+                        />
+                        <div>
+                            <h3 class="text-xl font-semibold">
+                                <a href="show.html">Senior Laravel Developer</a>
+                            </h3>
+                            <div class="text-lg font-bold mb-2">Acme Corp</div>
+                            <ul class="flex flex-wrap">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 mb-2 text-xs"
+                                >
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 mb-2 text-xs"
+                                >
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 mb-2 text-xs"
+                                >
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 mb-2 text-xs"
+                                >
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-base mt-2">
+                                <i class="fa-solid fa-location-dot"></i> Boston, MA
+                            </div>
+                        </div>
+                    </div>
             @endforeach
         </div>
     @else
         <p class="text-info">no job found</p>
     @endunless
-</x-card>
+{{-- </x-card> --}}
 
 
 
