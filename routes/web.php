18346a15ba7b377ec41c9d966f6d55e506728f9c
rahim8050 <rahimranxx8050@gmail.com>
2025-04-29 14:51:18 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,20 @@
 
 use App\Models\Work;
 use Inertia\Inertia;
+use App\Http\Controllers\Test;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
 Route::get('/',function(){
-    
+
     return view('users.jobs', [
         'jobs' => Work::all()
-         
-        
+
+
     ]);
-       
-     })->name('jobs'); 
+
+     })->name('jobs');
+     Route::get('/test', [Test::class, 'index'])->name('test');
      Route::get('/jobs/{id}',function($id){
         return view('users.job',[
             'job'=> Work::find($id)
